8152482591e0aa2d4a303a9243f688242995e521
courentin <ch2kn@free.fr>
2015-07-03 06:02:14 +0200

    Création des pages habitat, industrie

--- index.php
+++ index.php
@@ -6,28 +6,32 @@ $app = new \Slim\App();
 $container = $app->getContainer();
 
 $container->register(new \Slim\Views\Twig('public/view/', [
-    'cache' => 'cache/'
+    'cache' => 'cache/',
 ]));
 
 
-$app->get('/', function ($reqquest,$response,$args) {
+
+
+$app->get('/', function ($request,$response,$args) {
 	$imgs = [
 
 	];
     return $this->view->render($response, 'index.html', ['imgs', $imgs]);
 })->setName("index");
 
-$app->get('/habitat', function () {
 
+$app->get('/habitat', function ($request,$response,$args) {
+    return $this->view->render($response, 'habitat.html');
 })->setName("habitat");
 
-$app->get('/industrie', function () {
 
+$app->get('/industrie', function ($request,$response,$args) {
+    return $this->view->render($response, 'industrie.html');
 })->setName("industrie");
 
 
 $app->get('/contact/{type}', function ($request,$response,$args) {
-		echo $args['type'];
+    return $this->view->render($response, 'contact.html');
 	})
 	->setName("contact");
 
@@ -35,5 +39,9 @@ $app->post('/contact/{type}', function ($type = 'default') {
 		echo $type;
 	})
 	->setName("contact_post");
+
+
+
+
 
 $app->run();
